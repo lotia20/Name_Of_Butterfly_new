6e2ee4793185ba043d5fbf5f61cd703817fc1cb3
dong0one <dong0one9659@sookmyung.ac.kr>
2024-02-10 01:01:34 +0900

    1-2 player 이동 수정
    1-1도 1-2처럼 우클릭 없이 시점이동 수정하는 과정에서 상자 pw 클릭 할 때
    E키를 누르면 카메라가 상자를 비추도록 고정되는데
    이 때 마우스로 인해 제대로 고정이 안되는 오류 발생
    -> 일단 1-1씬에서는 우클릭으로 시점이동이 가능하도록 구현
    
    앉는 기능에서 카메라 위치 달라지는 오류 발생
    -> 추후 수정할 예정

---
+++ Assets/05.Scripts/1-2. Forest/Player.cs
@@ -0,0 +1,210 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //스피드 조정 변수
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField] 
+    private float runSpeed;
+    [SerializeField] 
+    private float crouchSpeed;
+    private float applySpeed;
+
+    //점프 정도
+    [SerializeField]
+     private float jumpForce;
+
+     //상태변수
+     private bool isRun = false;
+     private bool isGround = true;
+     private bool isCrouch = false;
+
+     //앉은 상태일 때 얼마나 앉을 지 결정하는 변수
+     [SerializeField] 
+     private float crouchPosY;
+     private float originPosY;
+     private float applyCrouchPosY;
+
+    [SerializeField]
+    private float lookSensitivity; 
+
+    [SerializeField]
+    private float cameraRotationLimit;  
+    private float currentCameraRotationX;  
+
+    [SerializeField]
+    private Camera camera; 
+    private Rigidbody rigidbody;
+    private CapsuleCollider capsuleCollider;
+
+    //zoom 변수 
+    private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
+    private float yRotate = 0.0f;
+    public float zoomSpeed = 10.0f;
+
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        //초기화
+        applySpeed = walkSpeed;
+
+        originPosY = camera.transform.localPosition.y;
+        Debug.Log(originPosY);
+        applyCrouchPosY = originPosY;
+    }
+
+    void Update()
+    {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
+        Move();
+        CameraRotation();
+        PlayerRotation();
+
+        Zoom();
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    // 점프
+    private void Jump()
+    {
+        if (isCrouch)
+            Crouch();
+
+        rigidbody.velocity = transform.up * jumpForce;
+    }  
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+     private void Running()
+    {
+        if (isCrouch)
+            Crouch();
+
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+        if (isCrouch) //앉은 상태
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    IEnumerator CrouchCoroutine()
+    {
+        float posY = camera.transform.localPosition.y;
+        int count = 0;
+
+        while(posY != applyCrouchPosY)
+        {
+            count++;
+            posY = Mathf.Lerp(posY, applyCrouchPosY, 0.2f);
+            camera.transform.localPosition = new Vector3(0, posY, 0);
+            if(count > 15)
+                break;
+            yield return null;
+        }
+        camera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0);
+    }
+
+
+    private void Move()
+    {
+        float moveDirX = Input.GetAxis("Horizontal");  
+        float moveDirY = Input.GetAxis("Vertical");
+        Vector3 moveHorizontal = transform.right * moveDirX; 
+        Vector3 moveVertical = transform.forward * moveDirY; 
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * applySpeed; 
+
+        rigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+    
+    private void CameraRotation()
+    {
+        float xRotation = Input.GetAxisRaw("Mouse Y"); 
+        float cameraRotationX = xRotation * lookSensitivity;
+        
+        currentCameraRotationX -= cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        camera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+    private void PlayerRotation()
+    {
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 playerRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(playerRotationY)); 
+    }
+
+    private void Zoom()
+    {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+        if(distance != 0)
+        {
+            float viewLimit = camera.fieldOfView + distance;
+            float minZoom = 30f;
+            float maxZoom = 80f;
+
+            camera.fieldOfView = Mathf.Clamp(viewLimit, minZoom, maxZoom);
+
+
+        }
+    } 
+}
